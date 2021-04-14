33a464ee723ab0d940bf2765f253715dfad84b99
javcol81 <blinkparana@gmail.com>
2021-04-14 00:14:33 -0300

    proyecto terminado

---
+++ database/seeders/PaymentPlatformsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\PaymentPlatform;
+use Illuminate\Database\Seeder;
+
+class PaymentPlatformsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      
+
+       
+
+        PaymentPlatform::create([
+            'name' => 'MercadoPago',
+            'image' => 'img/payment-platforms/mercadopago.jpg',
+        ]);
+
+       
+    }
+}
